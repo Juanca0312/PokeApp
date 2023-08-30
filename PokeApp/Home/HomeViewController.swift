1189dbd2535c24d5273d753d7327fff726982547
Juan Carlos Hernandez <jchernandez0312@gmail.com>
2023-08-30 13:19:09 -0500

    Initial Commit

---
+++ PokeApp/Home/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewController.swift
+//  PokeApp
+//
+//  Created by Juan Carlos Hernandez Castillo on 30/08/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemCyan
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
