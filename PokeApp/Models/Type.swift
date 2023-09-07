b439906fce7be19d6d1374d35d522a7934e61ed8
Juan Carlos Hernandez <jchernandez0312@gmail.com>
2023-09-07 01:14:00 -0500

    Improved Pokemon Type with enum type name

--- PokeApp/Models/Type.swift
+++ PokeApp/Models/Type.swift
@@ -13,6 +13,29 @@ struct `Type`: Codable {
 }
 
 struct TypeInfo: Codable {
-    let name: String
+    let name: TypeName
     let url: String
+}
+
+enum TypeName: String, Codable {
+    case normal
+    case fighting
+    case flying
+    case poison
+    case ground
+    case rock
+    case bug
+    case ghost
+    case steel
+    case fire
+    case water
+    case grass
+    case electric
+    case psychic
+    case ice
+    case dragon
+    case dark
+    case fairy
+    case `unknown`
+    case shadow
 }
